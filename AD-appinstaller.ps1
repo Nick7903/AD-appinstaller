6717fd1fe196db38c271d2a87b380338c03d26a3
Nick7903 <Nick7903@gmail.com>
2023-11-01 13:52:07 +0100

    Added -UseBasicParsing to Invoke-WebRequests

--- AD-appinstaller.ps1
+++ AD-appinstaller.ps1
@@ -8,8 +8,8 @@ Set-Location -Path $ENV:USERPROFILE
 
 try {Winget | Out-Null}
 catch {
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile VCLibs.appx; Add-AppxPackage -Path .\VCLibs.appx; Remove-Item -Path .\VCLibs.appx
-    Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Xaml.appx; Add-AppxPackage -Path .\Xaml.appx; Remove-Item -Path .\Xaml.appx
+    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile VCLibs.appx -UseBasicParsing; Add-AppxPackage -Path .\VCLibs.appx; Remove-Item -Path .\VCLibs.appx
+    Invoke-WebRequest -Uri "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx" -OutFile Xaml.appx -UseBasicParsing; Add-AppxPackage -Path .\Xaml.appx; Remove-Item -Path .\Xaml.appx
 
     $assets = (Invoke-WebRequest -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets"
 
