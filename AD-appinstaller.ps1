700304ad8d4fea0901ba2359f218fc5badaad7ec
Nick7903 <Nick7903@gmail.com>
2023-11-01 13:59:33 +0100

    Added -UseBasicParsing to Invoke-WebRequests 2

--- AD-appinstaller.ps1
+++ AD-appinstaller.ps1
@@ -11,7 +11,7 @@ catch {
     Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile VCLibs.appx -UseBasicParsing; Add-AppxPackage -Path .\VCLibs.appx; Remove-Item -Path .\VCLibs.appx
     Invoke-WebRequest -Uri "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx" -OutFile Xaml.appx -UseBasicParsing; Add-AppxPackage -Path .\Xaml.appx; Remove-Item -Path .\Xaml.appx
 
-    $assets = (Invoke-WebRequest -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets"
+    $assets = (Invoke-WebRequest -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest" -UseBasicParsing).Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets"
 
     $uri = $assets | Where-Object "browser_download_url" -Match '.msixbundle' | Select-Object -ExpandProperty "browser_download_url"; Invoke-WebRequest -Uri $uri -OutFile "Winget.msixbundle" -UseBasicParsing
     $uri = $assets | Where-Object "browser_download_url" -Match 'License\d.xml' | Select-Object -ExpandProperty "browser_download_url"; Invoke-WebRequest -Uri $uri -OutFile "Winget_License.xml" -UseBasicParsing
