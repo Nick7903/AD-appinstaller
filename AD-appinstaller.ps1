c3b8c3e07bd6b28623d8f15189af143eddd41da2
Nick7903 <Nick7903@gmail.com>
2023-11-01 09:23:21 +0100

    first

---
+++ AD-appinstaller.ps1
@@ -0,0 +1,16 @@
+
+Set-Location -Path $ENV:USERPROFILE
+
+try {Winget | Out-Null}
+catch {
+    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile VCLibs.appx; Add-AppxPackage -Path .\VCLibs.appx
+    Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Xaml.appx; Add-AppxPackage -Path .\Xaml.appx
+
+    $assets = (Invoke-WebRequest -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets"
+
+    $assets | Where-Object "browser_download_url" -Match '.msixbundle' | Select-Object -ExpandProperty "browser_download_url" | Invoke-WebRequest -Uri $_ -OutFile "Winget.msixbundle" -UseBasicParsing
+    $assets | Where-Object "browser_download_url" -Match 'License\d.xml' | Select-Object -ExpandProperty "browser_download_url" | Invoke-WebRequest -Uri $_ -OutFile "Winget_License.xml" -UseBasicParsing
+
+    Add-AppxProvisionedPackage -Online -PackagePath .\Winget.msixbundle -LicensePath .\Winget_License.xml
+}
+
